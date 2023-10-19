ad396a778563a1961f1385692c63a90d723b751e
Avneesh Kumar <146941069+avneesh86@users.noreply.github.com>
2023-10-19 22:44:18 +0530

    first commit 3

--- src/index.js
+++ src/index.js
@@ -10,6 +10,12 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+app.post('/webhooks/orders/create', (req, res) => {
+  console.log('🎉 We got an order!')
+  res.send('🎉 We got an order!');
+  // res.sendStatus(200)
+})
+
 app.listen(process.env.PORT, () =>
   console.log(`Example app listening on port ${process.env.PORT}!`),
 );
