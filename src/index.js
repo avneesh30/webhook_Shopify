ceb40240aadab7643990261fc43bf976651b5171
Avneesh Kumar <146941069+avneesh86@users.noreply.github.com>
2023-10-19 22:58:57 +0530

    first commit 4

--- src/index.js
+++ src/index.js
@@ -2,7 +2,12 @@ import 'dotenv/config';
 import cors from 'cors';
 import express from 'express';
 
+const secretKey = '28d9ba24dbbc3396b591bdeb89786581ebee23f452ac77d4b56359d404c1e4af'
+
 const app = express();
+
+const getRawBody = require('raw-body')
+const crypto = require('crypto')
 
 app.use(cors());
 
@@ -10,10 +15,28 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.post('/webhooks/orders/create', (req, res) => {
+app.post('/webhooks/orders/create', async (req, res) => {
   console.log('🎉 We got an order!')
-  res.send('🎉 We got an order!');
-  // res.sendStatus(200)
+
+
+  const hmac = req.get('X-Shopify-Hmac-Sha256')
+
+  const body = await getRawBody(req)
+
+  const hash = crypto
+  .createHmac('sha256', secretKey)
+  .update(body, 'utf8', 'hex')
+  .digest('base64')
+
+  if (hash === hmac) {
+    // It's a match! All good
+    console.log('Hello, it came from Shopify!')
+    res.sendStatus(200)
+  } else {
+    // No match! This request didn't originate from Shopify
+    console.log('Danger! Not from Shopify!')
+    res.sendStatus(403)
+  }
 })
 
 app.listen(process.env.PORT, () =>
